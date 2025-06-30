96c033e5470fd4817952a7d4d1acec78c998a154
Amr Galal Abdelraheem Ibrahim <amrgalalibrahim@gmail.com>
2025-06-30 19:08:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -199,5 +199,7 @@ def run_go_enrichment(gene_list):
     # Implementation would run actual GO analysis
     return [{'term': 'GO:0008150', 'pvalue': 0.001}]
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
